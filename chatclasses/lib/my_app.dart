9a7aa309d48451979b02adde4ab26f7073fa8574
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-08-11 20:43:25 -0300

    Configured Firebase on app

--- chatclasses/lib/my_app.dart
+++ chatclasses/lib/my_app.dart
@@ -10,8 +10,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primaryColor: Colors.deepPurple,
       ),
       home: const HomePage(),
     );
--- chatclasses/lib/pages/home_page.dart
+++ chatclasses/lib/pages/home_page.dart
@@ -8,12 +8,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var nicknameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
+      ),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        child: Column(children: [
+          const Text("Digite seu nickname"),
+          TextField(
+            controller: nicknameController,
+          ),
+          TextButton(onPressed: () {}, child: const Text("Selecionar sala"))
+        ]),
       ),
     ));
   }
